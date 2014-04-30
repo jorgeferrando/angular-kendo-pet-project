5f73b4bb1290dee6c6b0cb042d8aa9caaa46bd36
jorge <jorgeparent@gmail.com>
2014-04-30 13:09:50 +0200

    editable data table

--- js/modules/finance/module.js
+++ js/modules/finance/module.js
@@ -1,17 +1,11 @@
 define(["angular"], function (angular) {
     "use strict";
     return angular.module("finance", [])
-        .controller("financeCtrl", function ($scope,uni24Service) {
+        .controller("financeCtrl", function ($scope, uni24Service) {
             $scope.grid = {
                 options: {
-                    dataSource: [
-                        {
-                            ContactName: "Jorge",
-                            ContactTitle: "Utvikler",
-                            CompanyName: "Unimicro",
-                            Country: "Norway"
-                        }
-                    ],
+                    editable: "inline",
+                    toolbar: ["save", "cancel", "create"],
                     groupable: true,
                     sortable: true,
                     pageable: {
@@ -33,16 +27,37 @@ define(["angular"], function (angular) {
                         {
                             field: "Total",
                             title: "Total"
-                        }
+                        },
+                        { command: ["edit", "destroy"] }
                     ]
                 }
             };
+            $scope.data = [];
             uni24Service.getFlatInvoices().then(function (result) {
                 //onsole.log(result.Data);
+                $scope.data = result.Data;
                 var grid = $("#fmGrid").data("kendoGrid");
                 var dataSource = new kendo.data.DataSource({
-                    data: result.Data,
-                    pageSize: 10
+                    //data: result.Data,
+                    pageSize: 10,
+                    transport: {
+                        read: function(e) {
+                            e.success($scope.data);
+                        },
+                        update: function(e) {
+                            e.success();
+                        },
+                        create: function(e) {
+                            var item = e.data;
+                            item.AssignmentID = $scope.data.length + 1;
+                            e.success(item);
+                        }
+                    },
+                    schema: {
+                        model: {
+                            id: "AssignmentID"
+                        }
+                    }
                 });
                 grid.setDataSource(dataSource);
             });
